e443bf019049dd4588cfd67535c4197bf0c73796
Tamas Nagy <thomassocska@yahoo.com>
2018-04-20 15:53:22 +0200

    Aggregate all files into single files

---
+++ script/aggregate_clean_phys_data.R
@@ -0,0 +1,32 @@
+# Create one single clean file for the aggregated ECG and SCR data
+
+dir = "all_data/converted/"
+ecg_pattern = "_ECG.txt"
+scr_pattern = "_SCR.txt"
+
+# Aggregate all ECG data and write it in a single file
+all_ecg <- 
+    tibble(file = fs::dir_ls(dir, regexp = ecg_pattern)) %>% 
+    mutate(name = str_replace(file, str_glue(".*/(\\d+.*){pattern}$"),"\\1")) %>% 
+    separate(name, into = c("id","session"), extra = "merge") %>% 
+    mutate( data = map(file, ~read_tsv(file = .x, 
+                                       col_names = c("time", "ecg"), 
+                                       col_types = "dd")))
+
+all_ecg %>% 
+    unnest() %>% 
+    select(-file) %>% 
+    write_tsv("all_data/clean/all_ecg.tsv")
+    
+# Aggregate all SCR data and write it in
+all_scr <- 
+    tibble(file = fs::dir_ls(dir, regexp = scr_pattern)) %>% 
+    mutate(name = str_replace(file, str_glue(".*/(\\d+.*){pattern}$"),"\\1")) %>% 
+    separate(name, into = c("id","session"), extra = "merge") %>% 
+    mutate( data = map(file, ~read_tsv(file = .x, 
+                                       col_names = c("time", "scr"), 
+                                       col_types = "dd")))
+all_scr %>% 
+    unnest() %>% 
+    select(-file) %>% 
+    write_tsv("all_data/clean/all_scr.tsv")
